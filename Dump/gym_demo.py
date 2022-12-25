a0c93bd0524812cf1966aca08d0787e424fccdcd
[ Prabin Kumar Rath - Sch Elect Comptr Prath4 Energy Engr - Graduate Student - hyu24 ] <prath4@agave1.agave.rc.asu.edu>
2022-12-25 12:56:32 -0700

    update

--- Dump/gym_demo.py
+++ Dump/gym_demo.py
@@ -1,5 +1,6 @@
 import gym
-env = gym.make("PongDeterministic-v4")
+# import cv2
+env = gym.make("Pong-v4")
 env.metadata.setdefault('render_fps', 10)
 
 observation = env.reset()
@@ -7,7 +8,12 @@ done = False
 while not done:
     action = env.action_space.sample()
     observation, reward, done, info = env.step(action)
+    # gray = cv2.cvtColor(observation, cv2.COLOR_BGR2GRAY)  
+    # reshaped = cv2.resize(gray, (84,110))
+    # cropped = reshaped[18:102,:]
+    # cv2.imwrite('sample.png',cropped)
     print(reward, info)
+    # break
     if done:
         observation = env.reset()
 env.close()

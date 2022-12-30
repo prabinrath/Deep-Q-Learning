4d9c6a69b26404c5567bda3da7d847509a9ee5ab
Prabin Rath <prabinrath123@gmail.com>
2022-12-29 17:36:13 -0800

    update

--- Dump/gym_demo.py
+++ Dump/gym_demo.py
@@ -14,12 +14,12 @@ while not done:
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     cv2.imshow('Agent', img)
     cv2.waitKey(100)    
-    # gray = cv2.cvtColor(observation, cv2.COLOR_BGR2GRAY)  
+    # gray = cv2.cvtColor(observation, cv2.COLOR_BGR2GRAY)
     # reshaped = cv2.resize(gray, (84,110))
-    # cropped = reshaped[18:102,:]
-    # cv2.imwrite('sample.png',cropped)
+    # cropped = reshaped[18:102,:]/255
+    # cv2.imshow('Agent', cropped)
+    # cv2.waitKey(100)
     print(reward, info)
-    # break
     if done:
         observation = env.reset()
 env.close()
